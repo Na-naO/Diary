7b49cfb68178ebf27d99d0955eba863885dc46ec
Na-naO <la.dolce.vita.nana@gmail.com>
2021-01-03 21:57:26 +0900

    Add Diary app

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Diary;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateDiary;
+
+class DiaryController extends Controller
+{
+    public function index(){
+        // return 'Hello World';
+
+        //diariesテーブルのデータを全件取得
+        //useしてるDiaryのallメソッドを実施
+        //all()はテーブルのデータを全て取得するメソッド
+        $diaries = Diary::all();
+
+        // IDの降順にするときは
+        // $diaries = Diary::orderBy('id', 'desc')->get();
+
+        // var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
+        // dd($diaries); 
+
+        // view/diaries/index.blade.phpを表示
+        return view('diaries.index',['diaries' => $diaries]);
+    }
+
+    public function create()
+    {
+        return view('diaries.create');
+    }
+
+    public function store(CreateDiary $request)
+    {
+        $diary = new Diary();
+
+        $diary->title = $request->title;
+        $diary->body = $request->body;
+        $diary->save();
+
+        return redirect()->route('diary.index');
+
+        // dd('ここに保存処理');
+    }
+
+    public function destroy(int $id)
+    {
+        // Diaryモデルを使用して、diariesテーブルから$idと一致するidをもつデータを取得
+        $diary = Diary::find($id);
+
+        // 取得したデータを削除
+        $diary->delete();
+
+        return redirect()->route('diary.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'DiaryController@index')->name('diary.index');
+
+Route::get('diary/create', 'DiaryController@create')->name('diary.create');
+Route::post('diary/create', 'DiaryController@store')->name('diary.create');
+
+Route::delete('diary/{id}/delete', 'DiaryController@destroy')->name('diary.destroy');
+
+Route::get('diary/{id}/edit', 'DiaryController@edit')->name('diary.edit');
+Route::put('diary/{id}/update', 'DiaryController@update')->name('diary.update');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
