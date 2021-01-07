76cc0d9af1ac5c787a93630ead3d61449ed8b60c
Na-naO <la.dolce.vita.nana@gmail.com>
2021-01-07 23:45:38 +0900

    Add bootstrap, fix delete button and edit button

--- resources/views/diaries/create.blade.php
+++ resources/views/diaries/create.blade.php
@@ -5,21 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>新規投稿画面</title>
 </head>
 <body>
     <section class="container m-5">
         <div class="row justify-content-center">
             <div class="col-8">
+            @if($errors->any())
+                <ul>
+                    @foreach($errors->all() as $message)
+                    <li class="alert alert-danger">{{ $message }}</li>
+                    @endforeach
+                </ul>
+            @endif
                 <form action="{{ route('diary.create') }}" method="POST">
                     @csrf
                     <div class="form-group">
                         <label for="title">タイトル</label>
-                        <input type="text" class="form-control" name="title" id="title" />
+                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
                     </div>
                     <div class="form-group">
                         <label for="body">本文</label>
-                        <textarea class="form-control" name="body" id="body"></textarea>
+                        <textarea class="form-control" name="body" id="body">{{ old('body') }}</textarea>
                     </div>
                     <div class="text-right">
                         <button type="submit" class="btn btn-primary">投稿</button>
@@ -30,18 +38,10 @@
         </div>
     </section>
     
-    @if($errors->any())
-      <ul>
-        @foreach($errors->all() as $message)
-          <li class="alert alert-danger">{{ $message }}</li>
-        @endforeach
-      </ul>
-    @endif
-    <form action="{{ route('diary.create') }}" method="post">
-    <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
-    <textarea class="form-control" name="body" id="body">{{ old('body') }}</textarea>
-    </form>
+    
 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     
 </body>
 </html>
--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -7,6 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="/css/app.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>新規投稿画面</title>
   <title>一覧表示画面</title>
 </head>
 <body>
@@ -18,23 +20,17 @@
       <p>{{ $diary->title }}</p>
       <p>{{ $diary->body }}</p>
       <p>{{ $diary->created_at }}</p>
+      <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
+      <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
+        @csrf
+        @method('delete')
+        <button class="btn btn-danger">削除</button>
+      </form>
     </div>
   @endforeach
+  
 
-
-  <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
-  <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
-    @csrf
-    @method('delete')
-    <button class="btn btn-danger">削除</button>
-  </form>
-
-
-  <p>{{ $diary->created_at }}</p>
-  <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
-    @csrf
-    @method('delete')
-    <button class="btn btn-danger">削除</button>
-  </form>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </body>
 </html>
